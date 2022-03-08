40a1af83c933cc1e5e2014aa9f68b1b7df3dee84
meirbelo <90623098+meirbelo@users.noreply.github.com>
2022-03-08 09:52:15 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Reqct from 'react'
+import React from 'react'
 export default function App() {
   return (
     <View style={styles.container}>
